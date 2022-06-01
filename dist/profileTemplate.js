db7bab732ceecc48c7582995ca8b02d11318f5f3
Kaleb Stafford <kalebstafford0@gmail.com>
2022-05-31 21:42:16 -0500

    starting profileTemplate

---
+++ dist/profileTemplate.js
@@ -0,0 +1,9 @@
+function composeHtml(associate) {
+  return new Promise(function (resolve, reject) {
+    let Name = associate.fetchName();
+    let Role = associate.fetchRole();
+    let ID = associate.fetchId();
+    let emailAddress = associate.fetchEmail();
+    let data = "";
+  });
+}
